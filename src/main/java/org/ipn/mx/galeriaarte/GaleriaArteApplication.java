96b9be6906960820cc2bc245be32605bf607c5f1
ObedSG <osegurag2100@alumno.ipn.mx>
2025-11-05 20:31:39 -0600

    Iniciando Proyecto, Abrochar cinturones

---
+++ src/main/java/org/ipn/mx/galeriaarte/GaleriaArteApplication.java
@@ -0,0 +1,13 @@
+package org.ipn.mx.galeriaarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaArteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaleriaArteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ipn/mx/galeriaarte/GaleriaArteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ipn.mx.galeriaarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaArteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
